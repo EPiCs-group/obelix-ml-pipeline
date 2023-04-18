35f0ff3f150c9dbbea7d0a3f723a0ea35e457506
CValse <cvalsecc@awsaivirll002g.jnj.com>
2023-04-18 08:51:20 +0200

    Added heatmaps in all notebooks, closes #7

--- obelix_ml_pipeline/predictions_within_substrate_class.py
+++ obelix_ml_pipeline/predictions_within_substrate_class.py
@@ -10,34 +10,47 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 
-from obelix_ml_pipeline.load_representations import select_features_for_representation, load_and_merge_representations_and_experimental_response
+from obelix_ml_pipeline.load_representations import select_features_for_representation, load_and_merge_representations_and_experimental_response, load_ligand_representations,load_experimental_response,merge_dfs
 from obelix_ml_pipeline.machine_learning import prepare_classification_df, train_ml_model, predict_ml_model
 from obelix_ml_pipeline.data_classes import PredictionResults
 
-def predict_within_substrate_class(selected_ligand_representations, selected_substrate_representations,
+def predict_within_substrate_class(selected_ligand_representations,
                                     ligand_numbers_column, substrate_names_column, target, target_threshold, train_splits, binary,
                                     selected_substrate, training_size, rf_model, scoring, print_ml_results, n_jobs, plot_dendrograms=False):
     ligand_features = [select_features_for_representation(representation_type, ligand=True) for representation_type in
                        selected_ligand_representations]
     # flatten list of lists
     ligand_features = [item for sublist in ligand_features for item in sublist]
-    substrate_features = [select_features_for_representation(representation_type, ligand=False) for representation_type
-                          in selected_substrate_representations]
-    # flatten list of lists
-    substrate_features = [item for sublist in substrate_features for item in sublist]
-    # print(substrate_features)
-    features = ligand_features + substrate_features
+
+    features = ligand_features 
 
     # load selected representations and experimental response
-    df = load_and_merge_representations_and_experimental_response(selected_ligand_representations,
-                                                                  selected_substrate_representations, plot_dendrograms)
+    #df = load_and_merge_representations_and_experimental_response(selected_ligand_representations, plot_dendrograms)
+    selected_features = select_features_for_representation(selected_ligand_representations[0], ligand=True)
+    ligand_index_column = 'Ligand#'
+    selected_features_and_ligand_index = selected_features + [ligand_index_column]
+    first_ligand_rep = load_ligand_representations(selected_ligand_representations[0], columns_of_representation_to_select=selected_features_and_ligand_index)
+    exp_df = load_experimental_response()
+    df = merge_dfs(exp_df, ligand_index_column, first_ligand_rep, ligand_index_column)
+    if len(selected_ligand_representations) > 1:
+        for selected_ligand_representation in selected_ligand_representations[1:]:
+            selected_features = select_features_for_representation(selected_ligand_representation, ligand=True)
+            selected_features_and_ligand_index = selected_features + [ligand_index_column]
+            ligand_rep = load_ligand_representations(selected_ligand_representation, columns_of_representation_to_select=selected_features_and_ligand_index)
+            # keep merging ligand rep to first ligand rep and exp df
+            df = merge_dfs(df, ligand_index_column, ligand_rep, ligand_index_column)
+            
     # for the dataframe we want the ligand number, substrate name, target and ligand/substrate features
     df = df[[ligand_numbers_column, substrate_names_column, target] + features]
     if 'accuracy' in scoring:  # this means that we are doing a classification task
         df = prepare_classification_df(df, target, target_threshold, binary)
 
     subset_data = df.loc[df[substrate_names_column] == selected_substrate]
-    subset_train, subset_test = train_test_split(subset_data, test_size=1 - training_size, random_state=42, stratify = subset_data[target].values)
+    
+    if binary:
+        subset_train, subset_test = train_test_split(subset_data, test_size=1 - training_size, random_state=42, stratify = subset_data[target].values)
+    else:
+        subset_train, subset_test = train_test_split(subset_data, test_size=1 - training_size, random_state=42)
     train_data = subset_train
     best_model, training_best_model_performance, training_test_scores_mean, training_test_scores_std, fig_cm, fig_fi = train_ml_model(
         train_data, ligand_numbers_column, substrate_names_column,
@@ -60,7 +73,6 @@ def predict_within_substrate_class(selected_ligand_representations, selected_sub
 if __name__ == '__main__':
     # try classifier with loaded representations
     selected_ligand_representations = ['dft_nbd_model']
-    selected_substrate_representations = ['dft_steric_fingerprint']
     target = 'Conversion'
     target_threshold = 0.8
     rf_model = RandomForestClassifier(random_state=42)
@@ -76,7 +88,7 @@ if __name__ == '__main__':
     print_ml_results = True
     print('Training and testing classifier')
     print(f'Test size in training (based on K-fold): {1 / train_splits}')
-    prediction_results = predict_within_substrate_class(selected_ligand_representations, selected_substrate_representations,
+    prediction_results = predict_within_substrate_class(selected_ligand_representations,
                                     ligand_numbers_column, substrate_names_column, target, target_threshold, train_splits, binary,
                                     selected_substrate, training_size, rf_model, scoring, print_ml_results, n_jobs, plot_dendrograms)
     prediction_results.fig_cm.show()
@@ -92,7 +104,7 @@ if __name__ == '__main__':
     print('Training and testing regression')
     print(f'Test size: {1 / train_splits}')
     prediction_results = predict_within_substrate_class(
-        selected_ligand_representations, selected_substrate_representations,
+        selected_ligand_representations,
         ligand_numbers_column, substrate_names_column, target, target_threshold, train_splits, binary,
         selected_substrate, training_size, rf_model, scoring, print_ml_results, n_jobs, plot_dendrograms)
     prediction_results.testing_confusion_fig.show()
